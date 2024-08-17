02ab5bec091460a03725eae27455b628464348ff
Admin <2260391948@qq.com>
2024-08-18 00:25:50 +0800

    代码补充提交

---
+++ fronted/qidada-fronted/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <template v-if="route.path.startsWith('/user')">
+    <UserLayout />
+  </template>
+  <template v-else>
+    <BasicLayout />
+  </template>
+</template>
+
+<style scoped></style>
+<script setup lang="ts">
+import BasicLayout from "@/layouts/BasicLayout.vue";
+import { useRoute } from "vue-router";
+import UserLayout from "@/layouts/UserLayout.vue";
+import { onMounted } from "vue";
+const route = useRoute();
+/**
+ * 全局初始化函数，有全局单次调用的代码，都可以写到这里
+ */
+const doInit = () => {
+  console.log("hello 欢迎来到我的项目");
+};
+
+onMounted(() => {
+  doInit();
+});
+</script>
